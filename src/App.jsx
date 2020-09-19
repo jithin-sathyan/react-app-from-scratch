cc2fc7bb990e040554a273744de62eefcb4c9a6b
Jithin Sathyan <jithin@dexlock.com>
2020-09-19 18:30:19 +0530

    App and index files for react to start work

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <h1> Hello from React World! </h1>
+      </div>
+    );
+  }
+}
+
+export default App;
