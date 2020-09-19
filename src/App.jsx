b377edc7e55796037852aa6865f2c77913f3fb5a
Jithin Sathyan <jithin@dexlock.com>
2020-09-19 18:44:30 +0530

    Testing out hot reload options

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
+import { hot } from "react-hot-loader";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <h1> Hello from React World! </h1>
+        <h1> Hello User, Welcome to the React World! </h1>
       </div>
     );
   }
 }
 
-export default App;
+export default hot(module)(App);
